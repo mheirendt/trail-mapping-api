8e047f65681b396933227be294426d5f91cea335
mheirendt <michael.heirendt@gmail.com>
2016-10-14 13:17:01 -0600

    added facebook authentication

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, cb) {
     //User.findOrCreate({ facebookId: profile.id }, function (err, user) {
       //return cb(err, user);
-      return cb(null, user);
+      return cb(null, profile);
     //});
   }
 ));
