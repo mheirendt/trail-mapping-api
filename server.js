ae1b47b76fd9cc093bb1bde0fd13383ac67d5ea6
mheirendt <michael.heirendt@gmail.com>
2016-11-06 13:21:13 -0700

    removed orchestrate

--- server.js
+++ server.js
@@ -49,9 +49,7 @@ passport.deserializeUser(function(obj, done) {
 
 
 // Use the LocalStrategy within Passport to login/”signin” users.
-passport.use('local-signin', new LocalStrategy(
-    {passReqToCallback : true}, //allows us to pass back the request to the callback
-    function(req, username, password, email, done) {
+passport.use('local-signin', new LocalStrategy(function(username, password, email, done) {
 	funct.localAuth(username, password, email)
 	    .then(function (user) {
 		console.log("server.js local auth");
@@ -59,14 +57,14 @@ passport.use('local-signin', new LocalStrategy(
 		    console.log("We have a user in local Auth in server.js! : " + user);
 		    console.log(user.username);
 		    PROFILE_USERNAME = user.username;
-		    req.session.success = 'You are successfully logged in ' + user.username + '!';
+		    //req.session.success = 'You are successfully logged in ' + user.username + '!';
 		    console.log("Almost done with local auth in server.js");
 		    return done(null, user);
 		}
 		if (!user) {
 		    console.log("no users here in local Auth from Server.js");
 		    PROFILE_USERNAME = null;
-		    req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+		    //req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
 		    return done(null, user);
 		}
 	    })
