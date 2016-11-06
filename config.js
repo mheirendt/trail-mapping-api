c3b9535067922aadb2f32f75ca1284a6cece30eb
mheirendt <michael.heirendt@gmail.com>
2016-11-06 01:15:44 -0600

    removed orchestrate

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  'mongodb': 'mongodb://admin:s@l((tyD00b13@ds017246.mlab.com:17246/heroku_w8b74nd4'
+    //@ replaced with%40
+  'mongodb': 'mongodb://admin:salt((yD00b13%40ds017246.mlab.com:17246/heroku_w8b74nd4'
   //'db': '553c6928-ef20-4711-a806-8ba1b25b86a3'
 }
