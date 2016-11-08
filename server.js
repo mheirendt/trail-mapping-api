0afd2da427582f33b0f3739f48d54b4caa96e385
mheirendt <michael.heirendt@gmail.com>
2016-11-07 19:25:56 -0700

    removed orchestrate

--- server.js
+++ server.js
@@ -331,20 +331,15 @@ app.post('/local-reg', passport.authenticate('local-signup', {
   }));
 
 //sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
-app.post('/login', function(req, res, next) {
-    passport.authenticate('local-signin', function(err, user, info) {
-	if (err) { return next(err); }
-	if (!user) {
-	    return res.redirect('/signin');
-	} else {
-	    res.redirect('/');
-	}
-    });
-});
-	 //{
+app.post('/login', passport.authenticate('local-signin', function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/');
+    res.status(500).send('Sign in failed');
+    //{
     //successRedirect: '/',
     //failureRedirect: '/signin'
-//}));
+}));
 
 //logs user out of site, deleting them from the session, and returns to homepage
 app.get('/logout', function(req, res){
