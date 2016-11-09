03d56a092829515c7b1811c61ab93515653629e6
mheirendt <michael.heirendt@gmail.com>
2016-11-08 19:49:12 -0700

    removed orchestrate

--- server.js
+++ server.js
@@ -332,7 +332,7 @@ app.post('/local-reg', passport.authenticate('local-signup', {
   }));
 
 //sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
-app.post('/login', //function(req, res, next) {
+app.post('/login', function(req, res, next) {
   passport.authenticate('local-signin', function(err, user, info) {
       if (err) {
 	  //res.status(500).send(error);
