946b94b2a0d17929ef8cdc36c5831618214ef166
mheirendt <michael.heirendt@gmail.com>
2016-11-12 15:36:43 -0700

    big refactor

---
+++ config/passport.js
@@ -0,0 +1,43 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    //===============FACEBOOK================
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "https://secure-garden-50529.herokuapp.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, user, cb) {
+      PROFILE_USERNAME = user.displayName;
+      user.username = user.displayName;
+      return cb(null, user);
+  }
+));
+
+    //===============LOCAL STRATEGIES================
+    passport.use(new LocalStrategy(function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
+};
---
+++ models/user.js
@@ -0,0 +1,22 @@
+
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var md5 = require('MD5');
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    score: Number,
+    created: Date
+});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
