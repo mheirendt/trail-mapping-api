024d0d1b5f599089786fca7e6555d7a46a8c0521
mheirendt <michael.heirendt@gmail.com>
2016-11-13 16:17:13 -0700

    added trail.js schema

--- config/passport.js
+++ config/passport.js
@@ -8,9 +8,9 @@ module.exports = function(passport) {
     });
 
     passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user)
+        User.findById(id, function(err, user){
             done(err, user);
-        );
+        });
     
     });
 /*
