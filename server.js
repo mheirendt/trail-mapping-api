702c074b4a3ccd7b547d9d24e3da488178d66982
mheirendt <michael.heirendt@gmail.com>
2016-11-13 09:40:49 -0700

    added trail.js schema

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
     secret: 'saltydoob',
-    store: new MongoStore(db),
+    store: new MongoStore(mongooseConnection: mongoose.connection),
     cookie: { 
         secure: true
     }
