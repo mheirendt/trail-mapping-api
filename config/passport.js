c4e648490057ee69f6e767f96f0b0e805c10d35e
mheirendt <michael.heirendt@gmail.com>
2016-11-13 16:16:16 -0700

    added trail.js schema

--- config/passport.js
+++ config/passport.js
@@ -3,9 +3,9 @@ var User = require('../models/user');
 
 module.exports = function(passport) {
 
-    passport.serializeUser(function(user, done)
-        done(null, user.id);
-    );
+    passport.serializeUser(function(user, done) {
+	done(null, user.id);
+    });
 
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user)
