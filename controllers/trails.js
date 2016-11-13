d34cea1fa697b66477554f5b042aec51d232fe29
mheirendt <michael.heirendt@gmail.com>
2016-11-12 18:53:14 -0700

    added trail.js schema

---
+++ controllers/trails.js
@@ -0,0 +1,28 @@
+var Trail = require('../models/trail');
+var passport = require('passport');
+
+module.exports = {};
+
+module.exports.create = function(req, res) {
+    if (!req.body.categories || !req.body.tags || !req.body.geometry){
+        return res.status(400).end('Invalid input');
+    }
+
+     var newTrail = new Trail();
+     newTrail.categories = req.body.categories;
+     newTrail.tags = req.body.tags;
+     newTrail.geometry = req.body.geometry;
+     newTrail. submittedUser = "";
+     newTrail.created = new Date();
+
+
+     newTrail.save(function(error, trail){console.log(error + ", " + trail);});
+
+     res.writeHead(200, {"Content-Type": "application/json"});
+
+     newTrail = newTrail.toObject();
+     
+     res.end(JSON.stringify(newTrail));
+ 
+    });
+};
