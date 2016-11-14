b2fbdd19011e6db9e82d56ac5a1e5559aa25df6b
mheirendt <michael.heirendt@gmail.com>
2016-11-13 22:26:38 -0700

    added facebook routes

--- config/passport.js
+++ config/passport.js
@@ -17,61 +17,19 @@ module.exports = function(passport) {
         });
     
     });
-
+/*
     //===============FACEBOOK================
-    /*
-    passport.use(new FacebookStrategy({
-
-        // pull in our app id and secret from our auth.js file
-        clientID        : FACEBOOK_APP_ID,
-        clientSecret    : FACEBOOK_APP_SECRET,
-        callbackURL     : "https://secure-garden-50529.herokuapp.com/auth/facebook/callback",
-	// allows us to pass in the req from our route (lets us check if a user is logged in or not)
-        passReqToCallback : true 
-
-    },
-				      
-    function(req, token, refreshToken, profile, done) {
-        process.nextTick(function() {
-            if (!req.user) {
-                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-                    if (err)
-                        return done(err);
-                    if (user) {
-                        return done(null, user);
-                    } else {
-                        // if there is no user found with that facebook id, create them
-                        var newUser = new User();
-
-                        newUser.facebook.id    = profile.id;                   
-                        newUser.facebook.token = token;                   
-                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                        newUser.facebook.email = profile.emails[0].value;
-
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
-            }  else {
-                // user already exists and is logged in, we have to link accounts
-                var user = req.user;
-
-                user.facebook.id    = profile.id;
-                user.facebook.token = token;
-                user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                user.facebook.email = profile.emails[0].value;
-
-                user.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, user);
-                });
-            }
-	});
-    }));
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "https://secure-garden-50529.herokuapp.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, user, cb) {
+      PROFILE_USERNAME = user.displayName;
+      user.username = user.displayName;
+      return cb(null, user);
+  }
+));
 */
 
     //===============LOCAL STRATEGIES================
@@ -87,4 +45,6 @@ module.exports = function(passport) {
             }
             return done(null, user);
         });
-    }));
+    }
+));
+};
