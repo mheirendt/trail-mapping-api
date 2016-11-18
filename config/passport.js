c9d49823996ce44d04a97ee0152b9931bcf68156
mheirendt <michael.heirendt@gmail.com>
2016-11-17 18:47:05 -0700

    added facebook routes

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ passport.use(new FacebookStrategy({
     //===============LOCAL STRATEGIES================
     passport.use(new LocalStrategy(function(username, password, done) {
 	
-        User.findOne({ local.username: username }, function (err, user) {
+        User.findOne({ 'local.username': username }, function (err, user) {
             if (err) { return done(err); }
             if (!user) {
                 return done(null, false, { message: 'Incorrect username.' });
