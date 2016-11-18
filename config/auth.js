9c6ed09ea005a9e8d8bff457189c3f4ae43f901f
mheirendt <michael.heirendt@gmail.com>
2016-11-17 18:53:07 -0700

    added facebook routes

--- config/auth.js
+++ config/auth.js
@@ -4,4 +4,5 @@ module.exports = {
 		'clientID' 		: '143683242760890',
 		'clientSecret' 	: '095c264c52e8cd9001ab259070d5e971', // your App Secret
 		'callbackURL' 	: 'https://secure-garden-50529.herokuapp.com/auth/facebook/callback'
-	},
+	}
+}
