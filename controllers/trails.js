ea4621fb037027f919345516a9561703a3761ed1
mheirendt <michael.heirendt@gmail.com>
2017-01-02 16:09:50 -0700

    added post model, controller and routes

--- controllers/trails.js
+++ controllers/trails.js
@@ -1,5 +1,6 @@
 var Trail = require('../models/trail');
 var passport = require('passport');
+var Post = require('../models/post');
 
 module.exports = {};
 
@@ -7,6 +8,7 @@ module.exports.create = function(req, res) {
     if (!req.body.categories || !req.body.tags || !req.body.geometry){
         return res.status(400).end('Invalid input');
     }
+    //trail
     var newTrail = new Trail();
     //console.log("submitted user = " + req.user);
     newTrail.categories = req.body.categories;
@@ -18,7 +20,21 @@ module.exports.create = function(req, res) {
     newTrail.save(function(error, trail){
 	if (error)
 	    res.status(400).end('Could not save trail');
+	//post
+	var newPost = new Post();
+	newPost.submittedUsername = req.user;
+	newPost.submittedUser = trail.submittedUser;
+	newPost.reference = trail._id;
+	newPost.body = "Created a new trail.";
+	newPost.created = new Date();
+
+	newPost.save(function(error, post) {
+	    if (error)
+		res.status(400).end('Could not save post');
+	});
     });
+
+    
     res.writeHead(200, {"Content-Type": "application/json"});
     newTrail = newTrail.toObject();
     res.end(JSON.stringify(newTrail));
