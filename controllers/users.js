6f7b8b6fdce27ee3f9695a0e0c59ae7aba7d07e7
mheirendt <michael.heirendt@gmail.com>
2017-01-12 19:45:43 -0700

    added post model, controller and routes

--- controllers/users.js
+++ controllers/users.js
@@ -92,12 +92,12 @@ module.exports.findUsers = function(req, res) {
     //User.findOne({ username: req.params.username }, function(err, user) {
     console.log("username" + req.params.usernames);
     console.log(new RegExp(req.params.usernames, "i"));
+    var users = [];
     User.find({username : new RegExp(req.params.usernames, "i")})//, function (err, results) {
     .limit(10)
     .exec(function(err, results) {
 	if (err)
 	    return res.status(400).end('User not found');
-	var users = [];
 	//res.writeHead(200, {"Content-Type": "application/json"});
 	console.log("results: " + results);
 	results.forEach(function(user) {
@@ -106,8 +106,8 @@ module.exports.findUsers = function(req, res) {
             delete user.__v;
 	    users.push(user);
 	});
-	return res.end(users);
     });
+    return res.end(users);
 };
 
 //TODO refactor user schema and test
