21990f9695555101a5a01a3259b05829b6e87e2d
mheirendt <michael.heirendt@gmail.com>
2017-01-19 21:07:36 -0700

    added post model, controller and routes

--- controllers/gridFS.js
+++ controllers/gridFS.js
@@ -9,94 +9,49 @@ var upload = multer({dest: "./uploads"});
 //var formidable = require("formidable");
 
 
-//var gfs = new Grid(mongoose.connection.db);
-//var conn = mongoose.connection;
+var gfs = new Grid(mongoose.connection.db);
+Grid.mongo = mongoose.mongo;
  
 exports.create = function(req, res) {
-    console.log(JSON.stringify(req.file));
-    console.log("body: " + JSON.stringify(req.body));
+    //console.log(JSON.stringify(req.file));
+    //console.log("body: " + JSON.stringify(req.body));
 
     var dirname = "./";
-     var filename = req.file.name;
-     var path = req.file.path;
-     var type = req.file.mimetype;
+    var filename = req.file.name;
+    var path = req.file.path;
+    var type = req.file.mimetype;
       
-     var read_stream =  fs.createReadStream(dirname + '/' + path);
- 
-    var conn = req.conn;
-    Grid.mongo = mongoose.mongo;
- 
-    //var gfs = Grid(conn.db);
-    var gfs = new Grid(mongoose.connection.db);
-      
-     var writestream = gfs.createWriteStream({
+    var read_stream =  fs.createReadStream(dirname + '/' + path);
+    var writestream = gfs.createWriteStream({
         filename: filename
     });
     
     read_stream.pipe(writestream);
     
     read_stream.on('end', function () {
-        console.log('*** file upload finished');
+        res.status(200).end('File uploaded');
     });
     read_stream.on('error', function() {
-        console.log('*** error occured');
-        doCSUpdate();
+	res.status(400).end('There was a problem uploading image');
     });
  
 };
  
  
 exports.read = function(req, res) {
+    var pic_id = req.param('id');
+    var gfs = req.gfs;
  
-	gfs.files.find({ filename: req.params.filename }).toArray(function (err, files) {
- 
- 	    if(files.length===0){
-			return res.status(400).send({
-				message: 'File not found'
-			});
- 	    }
-	
-		res.writeHead(200, {'Content-Type': files[0].contentType});
-		
-		var readstream = gfs.createReadStream({
-			  filename: files[0].filename
-		});
- 
-	    readstream.on('data', function(data) {
-	        res.write(data);
-	    });
-	    
-	    readstream.on('end', function() {
-	        res.end();        
-	    });
- 
-		readstream.on('error', function (err) {
-		  console.log('An error occurred!', err);
-		  throw err;
-		});
-	});
- 
-};
- 
-/*
-    var busboy = new Busboy({ headers : req.headers });
-    var fileId = new mongo.ObjectId();
-
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-	console.log('got file', filename, mimetype, encoding);
-	var writeStream = gfs.createWriteStream({
-	    _id: fileId,
-	    filename: filename,
-	    mode: 'w',
-	    content_type: mimetype,
-	});
-	console.log("filename: " + filename);
-	file.pipe(writeStream);
-    }).on('finish', function() {
-	res.writeHead(200, {'content-type': 'text/html'});
-	console.log("fileID: " + fileId);
-	res.end(JSON.stringify(fileId));
+    gfs.files.find({filename: pic_id}).toArray(function (err, files) {
+        if (err)
+            res.end(err);
+        if (files.length > 0) {
+            var mime = 'image/jpeg';
+            res.set('Content-Type', mime);
+            var read_stream = gfs.createReadStream({filename: pic_id});
+            read_stream.pipe(res);
+        } else {
+            res.status(400).end('File Not Found');
+        }
     });
-
-    req.pipe(busboy);
-*/
+};
