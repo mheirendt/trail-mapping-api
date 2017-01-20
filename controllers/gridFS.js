c629e4688e5e38ccf3bdaf6becd841e677186a4a
mheirendt <michael.heirendt@gmail.com>
2017-01-19 21:15:34 -0700

    added post model, controller and routes

--- controllers/gridFS.js
+++ controllers/gridFS.js
@@ -2,36 +2,45 @@
  
 var mongoose = require('mongoose');
 var Grid = require('gridfs-stream');
+//var Busboy = require('busboy');
 var fs = require("fs");
 var multer = require("multer");
 var upload = multer({dest: "./uploads"});
+//var formidable = require("formidable");
 
 
-var gfs = new Grid(mongoose.connection.db);
+//var gfs = new Grid(mongoose.connection.db);
+//var conn = mongoose.connection;
+ 
 exports.create = function(req, res) {
     console.log(JSON.stringify(req.file));
     console.log("body: " + JSON.stringify(req.body));
 
     var dirname = "./";
-    var filename = req.file.name;
-    var path = req.file.path;
-    var type = req.file.mimetype;
+     var filename = req.file.name;
+     var path = req.file.path;
+     var type = req.file.mimetype;
       
-    var read_stream =  fs.createReadStream(dirname + '/' + path);
-
+     var read_stream =  fs.createReadStream(dirname + '/' + path);
+ 
+    var conn = req.conn;
     Grid.mongo = mongoose.mongo;
  
-    var writestream = gfs.createWriteStream({
+    //var gfs = Grid(conn.db);
+    var gfs = new Grid(mongoose.connection.db);
+      
+     var writestream = gfs.createWriteStream({
         filename: filename
     });
     
     read_stream.pipe(writestream);
     
     read_stream.on('end', function () {
-        res.status(200).end('Upload Successful');
+        console.log('*** file upload finished');
     });
-    read_stream.on('error', function(err) {
-	res.status(400).end(err);
+    read_stream.on('error', function() {
+        console.log('*** error occured');
+        doCSUpdate();
     });
  
 };
