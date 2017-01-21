fffdd1e81506e0e13994f17c1608e1a6b1d00c89
mheirendt <michael.heirendt@gmail.com>
2017-01-20 21:29:54 -0700

    added post model, controller and routes

--- controllers/gridFS.js
+++ controllers/gridFS.js
@@ -5,9 +5,6 @@ var Grid = require('gridfs-stream');
 var fs = require('fs');
 
 exports.create = function(req, res) {
-    console.log(JSON.stringify(req.file));
-    console.log(JSON.stringify(req.body));
-
     var dirname = "./",
 	filename = req.file.name,
 	path = req.file.path,
@@ -29,7 +26,7 @@ exports.create = function(req, res) {
 	console.log(writestream._id);
 	console.log(writestream.id);
 		    
-        res.status(200).end(JSON.stringify(writestream.id));
+        res.status(200).end(writestream.id);
     });
     read_stream.on('error', function(err) {
 	res.status(400).end(err);
