5a21c5e9be715a3cad12fcbbda57ebfdaa4ad224
mheirendt <michael.heirendt@gmail.com>
2017-01-20 21:16:43 -0700

    added post model, controller and routes

--- controllers/users.js
+++ controllers/users.js
@@ -196,6 +196,7 @@ module.exports.update = function(req, res) {
             if (user.username != req.user.username) {
                 return res.status(401).end('Modifying other user');
             } else {
+		console.log(JSON.stringify(req.body));
                 user.username = req.body.username ? req.body.username : user.username;
                 user.local.password = req.body.password ? user.generateHash(req.body.password) : user.password;
                 user.email = req.body.email ? req.body.email : user.email;
