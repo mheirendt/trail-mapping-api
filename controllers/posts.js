f9f91a12017c4a80e711929999498c56dd3123cc
mheirendt <michael.heirendt@gmail.com>
2017-01-31 10:10:22 -0700

    added post model, controller and routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -68,7 +68,7 @@ module.exports.getPosts = function (req, res) {
 		    .limit(2)
 		    .exec(function(err, posts) {
 			if (!err) {
-			    lastSeen = posts.slice(-1);//._id;//.created;
+			    lastSeen = posts.slice(-1)[0].created;//._id;//.created;
 			    var message = {
 				'posts': posts,
 				'lastSeen': lastSeen
