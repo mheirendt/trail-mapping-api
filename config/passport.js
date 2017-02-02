a32f71643de0c8eeab131a92046dec457b7e86a1
mheirendt <michael.heirendt@gmail.com>
2017-02-01 17:40:48 -0700

    added post model, controller and routes

--- config/passport.js
+++ config/passport.js
@@ -20,15 +20,15 @@ module.exports = function(passport) {
     passport.use(new LocalStrategy(function(username, password, done) {
         User.findOne({ 'username': username }, function (err, user) {
             if (err) {
-		return done(err);
+		done(err);
 	    }
             if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
+                done(null, false, { message: 'Incorrect username.' });
             }
             if (!user.validPassword(password)) {
-                return done(null, false, { message: 'Incorrect password.' });
+                done(null, false, { message: 'Incorrect password.' });
             }
-            return done(null, user);
+            done(null, user);
         });
     }));
 
