0bed9e6ddc68c66f0bd056081c4d12ce02617531
Michael Heirendt <michael.heirendt@gmail.com>
2019-07-14 12:27:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    //@ replaced with%40
-  'mongodb': 'mongodb://admin:salt((yD00b13%40ds017246.mlab.com:17246/heroku_w8b74nd4'
-  //'db': '553c6928-ef20-4711-a806-8ba1b25b86a3'
+  'mongodb': 'mongoDbConnectionString'
 }
